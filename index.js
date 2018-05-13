894d64a482814224a8a79ba0ec7ddf1b6e397884
Shun Yao <ysmiracle@hotmail.com>
2018-05-13 16:52:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+  return Math.abs(location - 42);
+}
+
+function distanceFromHqInFeet(location) {
+  return Math.abs(distanceFromHqInBlocks(location) * 264);
+}
+
+function distanceTravelledInFeet(begin, end) {
+  return Math.abs(distanceFromHqInFeet(begin) - distanceFromHqInFeet(end));
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance <= 2000) {
+    return distance * 0.02;
+  } else if (distance <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
